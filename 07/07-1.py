b0f7cbebe16bc23fa5efc5551a3ad4d1d6629ab5
Max <m@xic.email>
2021-12-12 14:31:54 +0000

    Completed day 7 🦀

---
+++ 07/07-1.py
@@ -0,0 +1,11 @@
+with open('07', 'r') as f: crabs = [int(x) for x in f.read().split(',')]
+#print(crabs)
+
+totals = {}
+for pos in range(0, max(crabs)):
+    pos_total = 0
+    for crab in crabs:
+        pos_total += abs(crab - pos)
+    totals[pos] = pos_total
+
+print(sorted(totals.items(), key = lambda i: i[1]))
